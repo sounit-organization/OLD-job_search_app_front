c00fe25d17a816c964e9892f2530ae55609a0a11
SOunit <workbookwork7@gmail.com>
2022-02-06 22:43:17 -0800

    add fetching data from mongo db

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,12 +1,29 @@
 const Test = () => {
-  const fetchDataHandler = () => {
+  const fetchDataHandler = async () => {
     console.log("fetch data from node");
+
+    // FIXME
+    // create http-request hook
+    const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/tests`);
+
+    if (!response.ok) {
+      return;
+    }
+
+    const responseData = await response.json();
+
+    console.log(responseData);
+  };
+
+  const createDataHandler = () => {
+    console.log("create data in node");
   };
 
   return (
     <div>
       <h1>Test</h1>
       <button onClick={fetchDataHandler}>Fetch Data from Node</button>
+      <button onClick={createDataHandler}>Create Test Data in Node</button>
     </div>
   );
 };
