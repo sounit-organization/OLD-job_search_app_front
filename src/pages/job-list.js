522b55eecdb6e3c563ff1afcc5bb9be0ffe650bd
SOunit <workbookwork7@gmail.com>
2022-02-08 00:19:48 -0800

    add header to job-list page

--- src/pages/job-list.js
+++ src/pages/job-list.js
@@ -1,19 +1,23 @@
-import { Fragment } from "react";
 import JobCardList from "../components/organisms/job-list/job-card-list";
 import JobSearch from "../components/organisms/job-list/job-search";
+import Header from "../components/organisms/shared/header";
+import classes from "./job-list.module.css";
 
 const JobList = () => {
   const dataList = [
-    { id: 1, title: "React Frontend Developer" },
-    { id: 2, title: "Node Backend Developer" },
+    { id: 1, title: "React Frontend Developer", skills: ["React", "Mongodb"] },
+    { id: 2, title: "Node Backend Developer", skills: ["Node.js"] },
   ];
 
   return (
-    <Fragment>
+    <div className={classes[componentName]}>
+      <Header />
       <JobSearch />
       <JobCardList jobList={dataList} />
-    </Fragment>
+    </div>
   );
 };
+
+const componentName = "JobList";
 
 export default JobList;
