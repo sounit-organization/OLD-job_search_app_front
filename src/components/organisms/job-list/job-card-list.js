336cd53255932fc6df4957ab349d323d617557a2
Azar Barfipour <azar.barfipour@gmail.com>
2022-03-10 20:32:43 -0800

    fix conflict

--- src/components/organisms/job-list/job-card-list.js
+++ src/components/organisms/job-list/job-card-list.js
@@ -1,11 +1,7 @@
 import Input from "../../atoms/input";
 import JobCard from "./job-card";
 import classes from "./job-card-list.module.css";
-<<<<<<< HEAD
-import Input from "../../atoms/input";
-=======
 import { useState } from "react";
->>>>>>> af990a2d988c312afef840cbd7fb91b1c468744b
 
 const componentName = "JobCardList";
 
@@ -13,12 +9,14 @@ const JobCardList = (props) => {
   const [searchTerm, setSearchTerm] = useState("");
 
   return (
-<<<<<<< HEAD
     <div>
       <div className={classes[componentName]}>
         <Input
           placeholder="Job title"
           className={classes[`${componentName}__input`]}
+          onChange={(event) => {
+            setSearchTerm(event.target.value);
+          }}
         />
         <Input
           placeholder="Vancouver, BC"
@@ -26,39 +24,24 @@ const JobCardList = (props) => {
         />
       </div>
       <ul className={classes[`${componentName}__list`]}>
-        {props.jobList.map((job) => (
-          <JobCard
-            key={job.id}
-            job={job}
-            className={classes[`${componentName}__card`]}
-          />
-        ))}
+        {props.jobList
+          .filter((val) => {
+            if (searchTerm == "") {
+              return val;
+            } else if (
+              val.title.toLowerCase().includes(searchTerm.toLowerCase())
+            ) {
+              return val;
+            }
+          })
+          .map((job) => (
+            <JobCard
+              key={job.id}
+              job={job}
+              className={classes[`${componentName}__card`]}
+            />
+          ))}
       </ul>
-=======
-    <div className={classes[componentName]}>
-
-      <input
-        type="text"
-        placeholder="Serch..."
-        onChange={(event) => {
-          setSearchTerm(event.target.value)
-        }} />
-
-      {props.jobList.filter((val) => {
-        if (searchTerm == "") {
-          return val;
-        } else if (val.title.toLowerCase().includes(searchTerm.toLowerCase())) {
-          return val;
-        }
-      }).map((job) => (
-        <JobCard
-          key={job.id}
-          job={job}
-          className={classes[`${componentName}__card`]}
-        />
-      ))}
-
->>>>>>> af990a2d988c312afef840cbd7fb91b1c468744b
     </div>
   );
 };
