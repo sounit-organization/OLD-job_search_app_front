c7a1f8304df0bb93777022c261becfd0dc39d4f8
Azar Barfipour <azar.barfipour@gmail.com>
2022-03-10 20:15:38 -0800

    added some styles

--- src/components/organisms/job-list/job-card-list.js
+++ src/components/organisms/job-list/job-card-list.js
@@ -1,18 +1,31 @@
 import JobCard from "./job-card";
 import classes from "./job-card-list.module.css";
+import Input from "../../atoms/input";
 
 const componentName = "JobCardList";
 
 const JobCardList = (props) => {
   return (
-    <div className={classes[componentName]}>
-      {props.jobList.map((job) => (
-        <JobCard
-          key={job.id}
-          job={job}
-          className={classes[`${componentName}__card`]}
+    <div>
+      <div className={classes[componentName]}>
+        <Input
+          placeholder="Job title"
+          className={classes[`${componentName}__input`]}
         />
-      ))}
+        <Input
+          placeholder="Vancouver, BC"
+          className={classes[`${componentName}__input`]}
+        />
+      </div>
+      <ul className={classes[`${componentName}__list`]}>
+        {props.jobList.map((job) => (
+          <JobCard
+            key={job.id}
+            job={job}
+            className={classes[`${componentName}__card`]}
+          />
+        ))}
+      </ul>
     </div>
   );
 };
